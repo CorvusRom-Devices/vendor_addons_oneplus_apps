ad24b74e357c650a216ec9a365d7e06f31b91abd
chandu078 <chandudyavanapelli03@gmail.com>
2021-02-25 03:34:10 +0000

    oneplus_apps: Nuke Overlay
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- config.mk
+++ config.mk
@@ -5,9 +5,6 @@ PRODUCT_PACKAGES += \
     OnePlusGallery \
     OnePlusFileManager
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-oem.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-oem.xml \
     $(LOCAL_PATH)/system/etc/permissions/privapp-permissions-op-filemanager.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-filemanager.xml \
